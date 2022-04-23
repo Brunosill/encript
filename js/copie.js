9033cf0d44fa9919ac6d88b6d504bfd6b1b15113
Bruno Silva <gsilvabruno619@gmail.com>
2022-04-23 10:23:25 -0300

    indo

---
+++ js/copie.js
@@ -0,0 +1,12 @@
+function botaoCopia(){
+    
+
+    var buttom = document.createElement("buttom");
+    var p = document.createElement("p");
+    p.textContent = "Copia texto";
+    buttom.appendChild(p);
+    buttom.classList.add("buttom");
+    buttom.classList.add("botao-copia");
+    buttom.addEventListener()
+    return buttom;
+}
